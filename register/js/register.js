470de96cf1ec222711ad1ae83a60c24e1e952bae
no_one <135714413+amrmohamed125@users.noreply.github.com>
2026-01-17 17:22:55 +0200

    Fix redirection URL after registration
    Updated the redirection URL for successful registration.

--- register/js/register.js
+++ register/js/register.js
@@ -19,8 +19,9 @@ registerbtn.addEventListener("click", function(e) {
     alert("Registration successful!");
 
     setTimeout(() => {
-      window.location.href = "login/login.html";
+      window.location.href = "../../login/login.html";
     }, 1000);
   }
 
 });
+

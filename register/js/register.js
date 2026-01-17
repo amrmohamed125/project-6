489445247b39e0da0c0daf8b19c68d35a5d57bce
no_one <135714413+amrmohamed125@users.noreply.github.com>
2026-01-17 17:26:14 +0200

    Fix registration redirection path

--- register/js/register.js
+++ register/js/register.js
@@ -19,9 +19,10 @@ registerbtn.addEventListener("click", function(e) {
     alert("Registration successful!");
 
     setTimeout(() => {
-      window.location.href = "../../login/login.html";
+      window.location.href = "../login/login.html";
     }, 1000);
   }
 
 });
+
 
